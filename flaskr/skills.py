6513a4c524f4a431215f5e74a0d3c03401643a35
Janne <janne.t.huhtanen@gmail.com>
2025-04-05 12:52:51 +0300

    change: changed the sql for joining the category and fixed the image missing from search. add: placeholder image when no image has been uploaded. change: some styling changes in the skills pages.

--- flaskr/skills.py
+++ flaskr/skills.py
@@ -183,12 +183,12 @@ def get_skill(skill_id):
         "SELECT s.ID, s.TITLE, s.DESCRIPTION, s.IS_FREE, s.PRICE, s.USER_ID, u.username AS username, "
         "(SELECT image_path FROM skill_images WHERE skill_images.skill_id = s.id LIMIT 1) AS image_path, "
         "c.title AS category, cv.value AS category_value, cv.category_id as category_id "
-        "FROM skills s, skill_categories sc "
+        "FROM skills s "
         "JOIN users u ON s.user_id = u.id "
         "JOIN category_values cv ON sc.category_value_id = cv.id "
         "JOIN categories c ON cv.category_id = c.id "
+        "JOIN skill_categories sc ON sc.skill_id = s.id "
         "WHERE "
-        "sc.skill_id = s.id AND "
         "s.id = ?", [skill_id]
     )
     return result[0] if result else None
